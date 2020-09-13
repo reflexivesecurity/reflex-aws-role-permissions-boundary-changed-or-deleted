fa7de89a99dfc1c598abaff33752eadc62ed64d2
rjulian <richard@rjulian.net>
2020-09-13 14:47:28 -0700

    Semi-automated multi account push

--- source/reflex_aws_role_permissions_boundary_changed_or_deleted.py
+++ source/reflex_aws_role_permissions_boundary_changed_or_deleted.py
@@ -2,7 +2,7 @@
 
 import json
 
-from reflex_core import AWSRule
+from reflex_core import AWSRule, subscription_confirmation
 
 
 class RolePermissionsBoundaryChangedOrDeleted(AWSRule):
@@ -46,6 +46,10 @@ class RolePermissionsBoundaryChangedOrDeleted(AWSRule):
 
 def lambda_handler(event, _):
     """ Handles the incoming event """
+    print(event)
+    if subscription_confirmation.is_subscription_confirmation(event):
+        subscription_confirmation.confirm_subscription(event)
+        return
     rule = RolePermissionsBoundaryChangedOrDeleted(
         json.loads(event["Records"][0]["body"])
     )
